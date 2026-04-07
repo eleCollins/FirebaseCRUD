184e91d71045664256641338f802a55e3503f1fd
Collins <A01285888@tec.mx>
2026-04-06 23:31:45 -0600

    CRUD app

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,59 @@
-import Image from "next/image";
+'use client'
+import { useEffect, useState } from 'react'
+import { addDoc, collection, getDocs, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from './../firebase/firebase.config';
 
 export default function Home() {
+  const [inputText, setInputText] = useState('');
+  const [items, setItems] = useState<any>([]);
+
+  useEffect(() => {
+    fetchItems()
+  }, [])
+
+  const fetchItems = async () => {
+    const snapshot = await getDocs(collection(db, 'items'))
+    setItems(snapshot.docs.map((doc) => ({ id: doc.id, inputText: doc.data().inputText })))
+  }
+
+  const handleAdd = async () => {
+    await addDoc(collection(db, 'items'), { inputText })
+    setInputText('');
+    fetchItems();
+  }
+
+  const handleDelete = async (id) => {
+    if (!id) return;
+    await deleteDoc(doc(db, 'items', id));
+    fetchItems();
+  }
+
+  const handleEdit = async (id) => {
+    const editValue = prompt("Enter the new value");
+    if (!editValue) return;
+    await updateDoc(doc(db, 'items', id), { inputText: editValue })
+    fetchItems();
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">NextJS Firebase CRUD</h1>
+      <input
+        type="text"
+        className="border-2 p-2 mr-2"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <button className="border p-2 bg-blue-500 text-white" onClick={handleAdd}>Agregar</button>
+      <ul className="mt-4">
+        {items.map((item) => (
+          <li key={item.id} className="mb-2">
+            {item.inputText}
+            <button className="p-2 border bg-yellow-500 text-white cursor-pointer ml-2" onClick={() => handleEdit(item.id)}>Edit</button>
+            <button className="p-2 border bg-red-500 text-white cursor-pointer ml-2" onClick={() => handleDelete(item.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
